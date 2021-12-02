9d4ee067fb361cd2d06ca0d84d282edff843ce8b
Erik Hansbo <erik.hansbo@seb.se>
2021-12-02 10:28:06 +0100

    day 2 prettier

--- d2.hs
+++ d2.hs
@@ -1,6 +1,5 @@
 import DayZero
 import Data.Char
-import Data.List.Split
 
 data Instruction = I Direction Int
     deriving (Show)
@@ -12,26 +11,24 @@ data Direction = Forward | Down | Up
 main :: IO ()
 main = do
     instructions <- manuallyParse "d2.txt" "\n" parseI
-    print $ solve instructions
-    print $ solve2 instructions
+    let (h, d) = foldl solve (0, 0) instructions
+    print $ h * d
+    let (h', d', _) = foldl solve2 (0, 0, 0) instructions
+    print $ h' * d'
 
 parseI :: String -> Instruction
-parseI s = I (read d') (read i)
-    where ((c:cs):i:[]) = splitOn " " s
-          d' = toUpper c : cs
+parseI s = I (read d) (read i)
+    where ((c:cs):i:[]) = words s
+          d = toUpper c : cs
 
+solve :: (Int, Int) -> Instruction -> (Int, Int)
+solve (h, d) (I dir i) = case dir of
+    Forward -> (h + i, d)
+    Up -> (h, d - i)
+    Down -> (h, d + i)
 
-solve :: [Instruction] -> Int
-solve = solve' (0, 0)
-    where solve' (h, d) ((I dir i):is)
-            | dir == Forward = solve' (h + i, d) is
-            | dir == Up = solve' (h, d - i) is
-            | dir == Down = solve' (h, d + i) is
-          solve' (h, d) _ = h*d
-
-solve2 = solve' (0, 0, 0)
-    where solve' (h, d, a) ((I dir i):is)
-            | dir == Forward = solve' (h + i, d + i * a, a) is
-            | dir == Up = solve' (h, d, a - i) is
-            | dir == Down = solve' (h, d, a + i) is
-          solve' (h, d, _) _ = h * d
+solve2 :: (Int, Int, Int) -> Instruction -> (Int, Int, Int)
+solve2 (h, d, a) (I dir i) = case dir of
+    Forward -> (h + i, d + i * a, a)
+    Up -> (h, d, a - i)
+    Down -> (h, d, a + i)
